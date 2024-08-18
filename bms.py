35970768fb54b307e59841241576d374187f52b3
Shingsam <35526270+shingsam@users.noreply.github.com>
2024-08-19 00:53:53 +0700

    Update bms.py

--- bms.py
+++ bms.py
@@ -1130,10 +1130,18 @@ while code_running == True:
     if bms_connected == True:
         if mqtt_connected == True:
 
+            success, data = bms_getAnalogData(bms,batNumber=1)
+            if success != True:
+                print("Error retrieving BMS analog data: " + data)
+            time.sleep(scan_interval/3)
+
             success, data = bms_getAnalogData(bms,batNumber=2)
             if success != True:
                 print("Error retrieving BMS analog data: " + data)
             time.sleep(scan_interval/3)
+
+
+            
             success, data = bms_getPackCapacity(bms)
             if success != True:
                 print("Error retrieving BMS pack capacity: " + data)
